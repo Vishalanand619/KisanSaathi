4769b0e424de15a5fcaa6ef5974023d67def28a9
Vishal Anand <anandvishal1302@gmail.com>
2026-06-15 21:21:23 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,3 @@
-// KisanSaathi - User Model
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -11,27 +10,26 @@ const userSchema = new mongoose.Schema(
     phone: { type: String },
     state: { type: String },
     district: { type: String },
-    // Farmer-specific fields
-    landHolding: { type: Number }, // in acres
+   
+    landHolding: { type: Number }, 
     cropType: { type: String },
     isActive: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 
-// Hash password before saving
+
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Compare password method
+
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// Remove password from JSON output
 userSchema.methods.toJSON = function () {
   const obj = this.toObject();
   delete obj.password;
