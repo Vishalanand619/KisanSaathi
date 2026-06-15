0dcdd89079a03823e47ed57721462ccacef9e151
Vishal Anand <anandvishal1302@gmail.com>
2026-06-15 21:31:39 +0530

    Update marketApi.js

--- frontend/src/api/marketApi.js
+++ frontend/src/api/marketApi.js
@@ -1,25 +1,20 @@
 import api from "./axios";
 
-// Get prices
+
 export const getMarketPrices = (params) =>
   api.get("/market", { params });
 
-// States dropdown
 export const getStates = () =>
   api.get("/market/states");
 
-// Crops dropdown
 export const getCrops = () =>
   api.get("/market/crops");
 
-// Add price (admin)
 export const addMarketPrice = (data) =>
   api.post("/market", data);
 
-// Delete
 export const deleteMarketPrice = (id) =>
   api.delete(`/market/${id}`);
 
-// Sync
 export const triggerSync = () =>
   api.post("/market/sync");
